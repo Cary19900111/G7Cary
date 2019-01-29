e633ade106642ad4cdf1d6c8ddfbd19cd6459c50
Cary <1516945385@qq.com>
2019-01-29 15:04:14 +0800

    level10 look seq

--- 20180716/learn.py
+++ 20180716/learn.py
@@ -123,7 +123,113 @@ def visit_page_continue(noth):
         except Exception as err:
             print(err)
             flag = False
+
 def test():
     pass
+
+def peak():
+    import urllib.request
+    import pickle
+    url ="http://www.pythonchallenge.com/pc/def/banner.p"
+    responce = urllib.request.urlopen(url)
+    text = responce.read().decode('utf-8')
+    str_result = ""
+    with open("./20180716/banner.p",'w') as f:
+        f.write(text)
+    with open("./20180716/banner.p",'rb') as f:
+        datas = pickle.load(f)
+    for data in datas:
+        for item in data:
+            str_result = str_result+item[0]*item[1]
+        str_result = str_result+'\n'
+    print(str_result)
+
+def channel(start_txt):
+    import zipfile
+    file_zip = zipfile.ZipFile("./channel.zip")
+    files_name = file_zip.namelist()
+    recy_flag = True
+    comment = ""
+    while recy_flag:
+        with file_zip.open(start_txt) as f:
+            content = f.read().decode("utf-8")
+            result = "{}.txt".format(content.split(" ").pop())
+            if result in files_name:
+                print(result)
+                comment = comment+file_zip.getinfo(start_txt).comment.decode("utf-8")
+                start_txt  = result
+                continue
+            else:
+                print(comment)
+                break
+
+def Image():
+    '''像素值:原稿某一小方块的平均亮度信息'''
+    from PIL import Image
+    import re
+    pic = Image.open("oxygen.png")
+    img1 = pic.crop((0,43,607,53)).convert("L")
+    char_pre = ""
+    str_result = ""
+    count = 0
+    for y in range (img1.size[0]):
+        char_current = chr(img1.getpixel((y,5)))
+        if char_current!=char_pre:
+            str_result = str_result+char_current
+            char_pre = char_current
+            count = 0
+        else:
+            #超过7次说明是一个新的数字
+            count = count+1
+            if count==8:
+                count = 0
+                str_result = str_result+char_current
+
+    pattern = re.compile(r"\d{1,3}")
+    pattern_result = pattern.findall(str_result)
+    print(pattern_result)
+    print("".join([chr(int(i)) for i in pattern_result]))
+    # print(new_pic.size)
+    # print(new_pic.mode)
+    # img1 = new_pic.crop((0,43,607,53)).convert("L")
+    # a= img1.getpixel((10,5))
+    #print(a)
+    # print(img1.size[0])
+
+def look_sequece(n):
+    b = lassseq()
+    for i in range(n):
+        result = next(b)
+    return result
+
+def list_to_str(list_list):
+    str_out = ""
+    for data_list in list_list:
+        count,num = data_list
+        str_out = str_out+str(count)+num
+    return str_out
+
+def lassseq():
+    num = '1'
+    while True:
+        try:
+            yield num
+            count = 0
+            current_char = ""
+            pre_char = ""
+            seq_list = []
+            for num_char in num:
+                if len(seq_list)>0:
+                    if num_char == seq_list[-1][1]:
+                        seq_list[-1][0] = seq_list[-1][0]+1
+                    else:
+                        seq_list.append([1,num_char])
+                else:
+                    seq_list.append([1,num_char])
+            num = list_to_str(seq_list)    
+        except Exception as err:
+            print(err)
+            break
+
 if __name__ == '__main__':
-    visit_page_continue("12345")
+    print(look_sequece(10))
